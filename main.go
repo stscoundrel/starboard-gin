c465c52d300500c03d802ed5759a91c117e6118b
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-11-19 16:29:32 +0200

    Add V2 stars endpoint with summary count
    Closes #1

--- main.go
+++ main.go
@@ -15,7 +15,12 @@ type StarStruct struct {
 	StarGazersLink string `json:"stargazer_link"`
 }
 
-func starsEndpoint(c *gin.Context) {
+type Summary struct {
+	Count   int
+	Starred []StarStruct
+}
+
+func starsEndpointV1(c *gin.Context) {
 	stars, starError := stars.GetStars(c.Param("username"))
 
 	if starError != nil {
@@ -30,16 +35,43 @@ func starsEndpoint(c *gin.Context) {
 	c.JSON(http.StatusOK, responseStars)
 }
 
+func starsEndpointV2(c *gin.Context) {
+	stars, starError := stars.GetStars(c.Param("username"))
+
+	if starError != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	response := Summary{
+		0,
+		[]StarStruct{},
+	}
+
+	for _, star := range stars {
+		starredRepo := StarStruct(star)
+		response.Count = response.Count + starredRepo.Count
+		response.Starred = append(response.Starred, starredRepo)
+	}
+	c.JSON(http.StatusOK, response)
+}
+
 func healthEndpoint(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "ok"})
 }
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
+
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/stars/:username", starsEndpoint)
+		v1.GET("/stars/:username", starsEndpointV1)
 		v1.GET("health", healthEndpoint)
+	}
+	v2 := router.Group("/v2")
+	{
+		v2.GET("/stars/:username", starsEndpointV2)
+		v2.GET("health", healthEndpoint)
 	}
 	router.Run()
 }
